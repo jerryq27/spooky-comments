a40f994fc57ac274209ba17ca72988f8e198cafc
Jerry Quintero <jerryq27@gmail.com>
2022-07-03 15:59:27 -0500

    Creating a comment component for each comment.

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@
 
 const url = 'http://localhost:8000/backend'
 
-const commentForm = document.querySelector('form')
-const input = document.getElementById('comment')
-const comments = document.getElementById('comments');
-
 // Get the comments and render list.
 document.onreadystatechange = () => {
     if(document.readyState === 'complete') {
@@ -14,6 +10,8 @@ document.onreadystatechange = () => {
 }
 
 getComments = () => {
+    const comments = document.getElementById('comments');
+
     // Make GET request.
     let xmlReq = new XMLHttpRequest();
     xmlReq.open("GET", `${url}/comments`, false);
@@ -22,8 +20,46 @@ getComments = () => {
 
     // Create list elements.
     json.forEach(comment => {
-        const listItem = document.createElement('li');    
-        listItem.innerHTML = comment['comment'];
+        const listItem = createCommentComponent(comment);
         comments.appendChild(listItem);
     });
+}
+
+createCommentComponent = (comment) => {
+    const container = document.createElement('li');
+    container.setAttribute('id', `comment-${comment.id}`)
+    container.setAttribute('class', 'comment');
+
+    const userSpan = document.createElement('span');
+    userSpan.setAttribute('class', 'user');
+
+    const timestampSpan = document.createElement('span');
+    timestampSpan.setAttribute('class', 'timestamp');
+
+    const commentText = document.createElement('p');
+    commentText.setAttribute('class', 'comment-text');
+
+    const upvoteCtrl = document.createElement('button');
+    upvoteCtrl.setAttribute('class', 'upvote');
+
+    const replyCtrl = document.createElement('button');
+    replyCtrl.setAttribute('class', 'reply');
+
+    const spacerSpan = document.createElement('span');
+
+    userSpan.innerText = comment.user;
+    spacerSpan.innerText = ' * ';
+    timestampSpan.innerText = comment.timestamp;
+    commentText.innerText = comment.comment;
+    upvoteCtrl.innerText = '^ Upvote';
+    replyCtrl.innerText = 'Reply';
+
+    container.appendChild(userSpan);
+    container.appendChild(spacerSpan);
+    container.appendChild(timestampSpan);
+    container.appendChild(commentText);
+    container.appendChild(upvoteCtrl);
+    container.appendChild(replyCtrl);
+
+    return container;
 }
