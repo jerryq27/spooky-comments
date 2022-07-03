8cceae2403fc5607281d4fe89c50c1b4feb5e88c
Jerry Quintero <jerryq27@gmail.com>
2022-07-03 17:32:25 -0500

    Expanded comment object data.
    Added fields for upvotes and avatars values to comments. Plus added more default values for the comment template.

--- app.js
+++ app.js
@@ -14,38 +14,47 @@ let comments = [
     {
         id: '1',
         user: 'Rob Hope',
+        avatar: 'img/sample-avatar-3.png',
         timestamp: '45 min ago',
         comment: 'Jeepers now that\'s a huge release with some big ' +
             'community earnings to back it - it must be so rewarding ' +
             'seeing creators quit their day jobs after monetizing ' +
             '(with real MRR) on the new platform.',
+        upvotes: 0,
         children: [],
     },
     {
         id: '2',
         user: 'Sophie Brecht',
+        avatar: 'img/sample-avatar-4.png',
         timestamp: 'Yesterday',
         comment: 'Switched our blog from Hubspot to Ghost a year ago -- ' +
             'turned out to be a great decision. Looking forward to this ' +
             'update....the in-platform analytics look expecially delicious. :)',
+        upvotes: 0,
         children: [
             {
                 id: '2-1',
                 user: 'James',
+                avatar: 'img/sample-avatar-2.png',
                 timestamp: '2 hrs ago',
                 comment: 'Thanks Sophie! Last year has been an absolut goldrush for ' +
                     'the creator economy. Slowly at first, then all at once. Will be ' +
-                    'interesting to see how this ecosystem evolves over the next few years'
+                    'interesting to see how this ecosystem evolves over the next few years',
+                upvotes: 0,
+                children: [],
             },
         ]
     },
     {
         id: '3',
         user: 'Cameron Lawrence',
+        avatar: 'img/sample-avatar-1.png',
         timestamp: '3 weeks ago',
         comment: 'Love the native memberships and the zipless themes, I was ' +
             'just asked by a friend about the options for a new site, and I ' +
             'think I know what I\'ll be recommending then...',
+        upvotes: 0,
         children: [],
     }
 ];
@@ -53,8 +62,10 @@ let comments = [
 let commentTemplate = {
     id: null,
     user: 'Rob Hope',
-    timestamp: '',
+    avatar: 'img/sample-avatar-3.png',
+    timestamp: 'Just now',
     comment: '',
+    upvotes: 0,
     children: []
 };
 
