fb162350d39c0bef51e9175f9beee8f6e7db2bc2
Jerry Quintero <jerryq27@gmail.com>
2022-07-04 20:41:28 -0500

    Switched avatar images.
    Added Halloween themed icons from Flaticon, changed the comment data to new images, and included footer link to use the images.

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 let comments = [
     {
         id: '1',
-        user: 'Rob Hope',
-        avatar: 'img/sample-avatar-3.png',
+        user: 'Greg Ghost',
+        avatar: 'img/spooky/avatar-ghost.png',
         timestamp: '45 min ago',
         comment: 'Jeepers now that\'s a huge release with some big ' +
             'community earnings to back it - it must be so rewarding ' +
@@ -27,8 +27,8 @@ let comments = [
     },
     {
         id: '2',
-        user: 'Sophie Brecht',
-        avatar: 'img/sample-avatar-4.png',
+        user: 'Reaper Randy',
+        avatar: 'img/spooky/avatar-reaper.png',
         timestamp: 'Yesterday',
         comment: 'Switched our blog from Hubspot to Ghost a year ago -- ' +
             'turned out to be a great decision. Looking forward to this ' +
@@ -37,8 +37,8 @@ let comments = [
         children: [
             {
                 id: '2-1',
-                user: 'James',
-                avatar: 'img/sample-avatar-2.png',
+                user: 'Mummy',
+                avatar: 'img/spooky/avatar-mummy.png',
                 timestamp: '2 hrs ago',
                 comment: 'Thanks Sophie! Last year has been an absolut goldrush for ' +
                     'the creator economy. Slowly at first, then all at once. Will be ' +
@@ -50,8 +50,8 @@ let comments = [
     },
     {
         id: '3',
-        user: 'Cameron Lawrence',
-        avatar: 'img/sample-avatar-1.png',
+        user: 'Vampire Vlad',
+        avatar: 'img/spooky/avatar-dracula.png',
         timestamp: '3 weeks ago',
         comment: 'Love the native memberships and the zipless themes, I was ' +
             'just asked by a friend about the options for a new site, and I ' +
@@ -63,8 +63,8 @@ let comments = [
 
 let commentTemplate = {
     id: null,
-    user: 'Rob Hope',
-    avatar: 'img/sample-avatar-3.png',
+    user: 'Greg Ghost',
+    avatar: 'img/spooky/avatar-ghost.png',
     timestamp: 'Just now',
     comment: '',
     upvotes: 0,
